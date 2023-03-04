216bb44dfd0a20fe3701f8e5461ad0ee4bcd97e4
hiven <mail@hiven.com>
2023-03-04 08:49:13 +0000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,10 @@ import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask import Flask, request, current_app
-from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from elasticsearch import Elasticsearch
 from config import Config
 
-db = SQLAlchemy()
 bootstrap = Bootstrap()
 
 
@@ -15,7 +13,6 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    db.init_app(app)
     bootstrap.init_app(app)
     app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
         if app.config['ELASTICSEARCH_URL'] else None
