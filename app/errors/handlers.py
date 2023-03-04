4da0baf2a84f714bd45cce955343435e609a8ec1
hiven <mail@hiven.com>
2023-03-04 15:07:45 +0000

    Update handlers.py

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -1,5 +1,4 @@
 from flask import render_template, request
-from app import db
 from app.errors import bp
 from app.api.errors import error_response as api_error_response
 
@@ -18,7 +17,6 @@ def not_found_error(error):
 
 @bp.app_errorhandler(500)
 def internal_error(error):
-    db.session.rollback()
     if wants_json_response():
         return api_error_response(500)
     return render_template('errors/500.html'), 500
