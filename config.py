1e0956d1a4fd5b16299779364b4cff4a0a5576aa
hiven <mail@hiven.com>
2023-03-04 10:45:17 +0000

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    ELASTICSEARCH_URL = 'http://109.205.61.211'
+    ELASTICSEARCH_URL = 'http://109.205.61.211:80'
     POSTS_PER_PAGE = 25
